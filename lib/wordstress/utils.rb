4b734256e4613d3b72bd82c10baca535f971d0fb
Paolo Perego <thesp0nge@gmail.com>
2015-01-19 18:33:06 +0100

    Build a good per day and per scan directory name to store results

--- lib/wordstress/utils.rb
+++ lib/wordstress/utils.rb
@@ -3,7 +3,25 @@ module Wordstress
 
     # Transform a given URL into a directory name to be used to store data
     def self.target_to_dirname(target)
-      target.split("://")[1].gsub('.','_').gsub('/', '')
+      target.split("://")[1].gsub('.','_').gsub('/', '').gsub(':', '_')
+    end
+
+    def self.build_output_dir(root, target)
+      attempt=0
+      today=Time.now.strftime("%Y%m%d")
+
+      while 1 do
+
+        proposed = File.join(root, Wordstress::Utils.target_to_dirname(target), today)
+        if attempt != 0
+          proposed += "_#{attempt}"
+        end
+
+        return proposed unless Dir.exists?(proposed)
+        attempt +=1 if Dir.exists?(proposed)
+      end
+
+
     end
 
   end
